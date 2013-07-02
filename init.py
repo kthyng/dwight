fdccd2a5e33358d6de8eaa7aaec9a77d7a97e0c8
Kristen M. Thyng <kthyng@gmail.com>
2013-07-02 18:58:44 -0500

    Worked on form for the functions
    Getting things set up for running the simulations, and also put all of the package locations, lat/lons, and times into init functions.

--- init.py
+++ init.py
@@ -9,10 +9,9 @@ import pdb
 import glob
 from datetime import datetime, timedelta
 from matplotlib.mlab import *
-import inout
-import tools
 
-def parameters(loc,nsteps,ndays,ff,tseas,ah,av,z0,zpar,do3d,doturb):
+
+def parameters():
 	'''
 	Parameters for running numerical simulation.
 	loc 	Path to directory of grid and output files
@@ -84,13 +83,7 @@ def parameters(loc,nsteps,ndays,ff,tseas,ah,av,z0,zpar,do3d,doturb):
 	tseas = 4*3600 # 4 hours between outputs, in seconds, time between model outputs 
 	ah = 5. #100.
 	av = 1.e-5 # m^2/s, or try 5e-6
-
-	# grid = netCDF.Dataset(loc+'grid.nc')
-	# lonr = grid.variables['lon_rho'][:]
-	# latr = grid.variables['lat_rho'][:]
-	grid = inout.readgrid(loc)
-
-
+=
 	## Choose method for vertical placement of drifters
 	# Also update makefile accordingly. Choose the twodim flag for isoslice.
 	# See above for more notes, but do the following two lines for an isoslice
@@ -109,41 +102,23 @@ def parameters(loc,nsteps,ndays,ff,tseas,ah,av,z0,zpar,do3d,doturb):
 	# doturb=3 means adding diffusion on an ellipse (anisodiffusion)
 	doturb = 0
 
-	# simulation name, used for saving results into netcdf file
-	name = 'temp_mapcoord_order1' #'5_5_D5_F'
-
-	# # Save these settings to a file
-	# np.savez('tracks/' + name + 'header.in',loc=loc,nsteps=nsteps,ndays=ndays, 
-	# 		ff=ff,date=date,tseas=tseas,ah=ah,av=av,lon0=lon0,lat0=lat0,
-	# 		z0=z0,zpar=zpar,do3d=do3d,doturb=doturb,name=name)
-
-	return loc,nsteps,ndays,ff,date,tseas,ah,av,lon0,lat0,z0,zpar,do3d,doturb,name
+	return loc,nsteps,ndays,ff,tseas,ah,av,z0,zpar,do3d,doturb
 
 
-def locations():
+def locations(test):
 	'''
-	lon0 	Drifter starting locations in x/zonal direction.
-	lat0 	Drifter starting locations in y/meridional direction.
-	name	Name of simulation to be used for netcdf file containing final tracks
+	Contains the locations and name information for the simulations.
+
+	Inputs:
+		test 	Index of the test case we want to run
+
+	Outputs:
+		lon0 	Drifter starting locations in x/zonal direction for test
+		lat0 	Drifter starting locations in y/meridional direction for test
+		name	Name of simulation to be used for netcdf file containing final tracks for test
 	'''
 
-	## Input starting locations as real space lon,lat locations
-	# lon0,lat0 = np.meshgrid(-95.498218005315309,23.142258627126882) # [0,0] (SE) corner
-	# lon0,lat0 = np.meshgrid(-97.748582291691989,23.000027311710628) # [-1,0] (SW) corner
-	# lon0,lat0 = np.meshgrid(-87.757124031927574,29.235771320764623) # [0,-1] (NE) corner
-	# lon0,lat0 = np.meshgrid(-88.3634073986196,30.388542615201313) # [-1,-1] (NW) corner
-	# lon0,lat0 = np.meshgrid(np.linspace(-94,-93,10),np.linspace(28,29,10)) # grid outside Galveston Bay
-	# lon0,lat0 = np.meshgrid(np.linspace(-95,-91,100),np.linspace(28,29,50)) # rectangle outside Galveston
-
-	# lon0,lat0 = np.meshgrid(np.linspace(-98.5,-87.5,1100),np.linspace(22.5,31,980)) # whole domain, 1 km
-	# lon0,lat0 = np.meshgrid(np.linspace(-98.5,-87.5,220),np.linspace(22.5,31,196)) # whole domain, 5 km
-	# FOR TEST1:
-	lon0,lat0 = np.meshgrid(np.linspace(-98.5,-87.5,110),np.linspace(22.5,31,98)) # whole domain, 10 km
-	# lon0,lat0 = np.meshgrid(np.linspace(-98.5,-87.5,21),np.linspace(22.5,31,20)) # whole domain, 50 km
-
-	# Eliminate points that are outside domain or in masked areas
-	lon0,lat0 = tools.check_points(lon0,lat0,grid)
-
+	# Name of locations packages were found
 	name = list((
 			'Matagorda Island, TX',
 			'Padre Island National Seashore, TX',
@@ -186,93 +161,150 @@ def locations():
 			'Cameron, LA',
 			'Cameron, LA'))
 
-	lat =  28°10'8.03"N
-	27°25'26.96"N
-	28°10'8.03"N
-	np.nan
-	 27°44'21.98"N
-	27°25'26.96"N
-	 26° 6'42.62"N
-	27°25'26.96"N
-	27°25'26.96"N
-	 27°44'21.98"N
-	27°25'26.96"N
-	27°25'26.96"N
-	 30°12'40.09"N
-	 np.nan
-	  29°16'16.79"N
-	  29°16'16.79"N
-	  29°27'25.82"N
-	   29°32'33.32"N
-	  29°16'16.79"N
-	  29°34'0.83"N
-	  29°18'8.85"N
-	  29°27'25.82"N
-	  29°39'24.04"N
-	  29°41'40.00"N
-	   27°15'49.24"N
-	  29°39'24.04"N
-	  29°39'24.04"N
-	  29°16'16.79"N
-	  29°41'3.88"N
-	  29°41'3.88"N
-	  29°41'3.88"N
-	  29°41'3.88"N
-	  29°41'12.84"N
-	  29°41'12.84"N
-	  29°12'15.35"N
-	  29°28'41.84"N
-	  np.nan
-	  29°47'51.80"N
-	  29°47'51.80"N
-	  29°47'51.80"N
+	# Latitudes of where packages were found
+	lat = np.array([28 + 10/60. + 8.03/3600.,
+					27 + 25/60. + 26.96/3600.,
+					28 + 10/60. + 8.03/3600.,
+					np.nan,
+					27 + 44/60. + 21.98/3600.,
+					27 + 25/60. + 26.96/3600.,
+					26 + 6/60. + 42.62/3600.,
+					27 + 25/60. + 26.96/3600.,
+					27 + 25/60. + 26.96/3600.,
+					27 + 44/60. + 21.98/3600.,
+					27 + 25/60. + 26.96/3600.,
+					27 + 25/60. + 26.96/3600.,
+					30 + 12/60. + 40.09/3600.,
+					np.nan,
+					29 + 16/60. + 16.79/3600.,
+					29 + 16/60. + 16.79/3600.,
+					29 + 27/60. + 25.82/3600.,
+					29 + 32/60. + 33.32/3600.,
+					29 + 16/60. + 16.79/3600.,
+					29 + 34/60. + 0.83/3600.,
+					29 + 18/60. + 8.85/3600.,
+					29 + 27/60. + 25.82/3600.,
+					29 + 39/60. + 24.04/3600.,
+					29 + 41/60. + 40.00/3600.,
+					27 + 15/60. + 49.24/3600.,
+					29 + 39/60. + 24.04/3600.,
+					29 + 39/60. + 24.04/3600.,
+					29 + 16/60. + 16.79/3600.,
+					29 + 41/60. + 3.88/3600.,
+					29 + 41/60. + 3.88/3600.,
+					29 + 41/60. + 3.88/3600.,
+					29 + 41/60. + 3.88/3600.,
+					29 + 41/60. + 12.84/3600.,
+					29 + 41/60. + 12.84/3600.,
+					29 + 12/60. + 15.35/3600.,
+					29 + 28/60. + 41.84/3600.,
+					np.nan,
+					29 + 47/60. + 51.80/3600.,
+					29 + 47/60. + 51.80/3600.,
+					29 + 47/60. + 51.80/3600.])
 
-	lon =  96°44'19.30"W
-	 97°17'57.06"W
-	 96°44'19.30"W
-	 np.nan
-	  97° 7'54.01"W
-	 97°17'57.06"W
-	  97°10'5.25"W
-	 97°17'57.06"W
-	 97°17'57.06"W
-	  97° 7'54.01"W
-	 97°17'57.06"W
-	 97°17'57.06"W
-	  88°57'50.85"W
-	  np.nan
-	  94°49'25.87"W
-	  94°49'25.87"W
-	   94°38'22.71"W
-	    95° 0'56.82"W
-	  94°49'25.87"W
-	  94°23'36.68"W
-	   94°46'27.70"W
-	   94°38'22.71"W
-	   94° 6'20.05"W
-	   93°51'9.07"W
-	   97°21'49.13"W
-	   94° 6'20.05"W
-	   94° 6'20.05"W
-	   94°49'25.87"W
-	   93°51'2.66"W
-	   93°51'2.66"W
-	   93°51'2.66"W
-	   93°51'2.66"W
-	   94° 2'38.40"W
-	   94° 2'38.40"W
-	   94°56'10.69"W
-	   94°34'47.69"W
-	   np.nan
-	   93°19'30.16"W
-	   93°19'30.16"W
-	   93°19'30.16"W
+	# Longitudes of where packages were found
+	lon = np.array([96 + 44/60. + 19.30/3600.,
+					97 + 17/60. + 57.06/3600.,
+					96 + 44/60. + 19.30/3600.,
+					np.nan,
+					97 +  7/60. + 54.01/3600.,
+					97 + 17/60. + 57.06/3600.,
+					97 + 10/60. + 5.25/3600.,
+					97 + 17/60. + 57.06/3600.,
+					97 + 17/60. + 57.06/3600.,
+					97 +  7/60. + 54.01/3600.,
+					97 + 17/60. + 57.06/3600.,
+					97 + 17/60. + 57.06/3600.,
+					88 + 57/60. + 50.85/3600.,
+					np.nan,
+					94 + 49/60. + 25.87/3600.,
+					94 + 49/60. + 25.87/3600.,
+					94 + 38/60. + 22.71/3600.,
+					95 +  0/60. + 56.82/3600.,
+					94 + 49/60. + 25.87/3600.,
+					94 + 23/60. + 36.68/3600.,
+					94 + 46/60. + 27.70/3600.,
+					94 + 38/60. + 22.71/3600.,
+					94 +  6/60. + 20.05/3600.,
+					93 + 51/60. + 9.07/3600.,
+					97 + 21/60. + 49.13/3600.,
+					94 +  6/60. + 20.05/3600.,
+					94 +  6/60. + 20.05/3600.,
+					94 + 49/60. + 25.87/3600.,
+					93 + 51/60. + 2.66/3600.,
+					93 + 51/60. + 2.66/3600.,
+					93 + 51/60. + 2.66/3600.,
+					93 + 51/60. + 2.66/3600.,
+					94 +  2/60. + 38.40/3600.,
+					94 +  2/60. + 38.40/3600.,
+					94 + 56/60. + 10.69/3600.,
+					94 + 34/60. + 47.69/3600.,
+					np.nan,
+					93 + 19/60. + 30.16/3600.,
+					93 + 19/60. + 30.16/3600.,
+					93 + 19/60. + 30.16/3600.])
 
-def dates():
+	# Select out the lon/lat for test
+	lon0,lat0 = np.meshgrid(np.linspace(lon[test]-0.1, lon[test]+0.1,30), \
+							np.linspace(lat[test]+0.1, lat[test]+0.1,30))
+
+	# Eliminate points that are outside domain or in masked areas
+	lon0,lat0 = tools.check_points(lon0,lat0,grid)
+
+	return lon0, lat0, name[test]
+
+
+def start_times(test):
 	'''
-	date 	Start date in datetime object
+	Contains the times for finding the locations.
+
+	Inputs:
+		test 	Index of the test case we want to run
+
+	Outputs:
+		date 	Start date for test in datetime object
 	'''
 
-	# Start date
-	date = datetime(2009,11, 25, 0)
+	dates = np.array([datetime(2013, 1, 11, 0),
+					datetime(2013, 1, 16, 0),
+					datetime(2013, 1, 18, 0),
+					datetime(2013, 1, 25, 0),
+					datetime(2013, 1, 29, 0),
+					datetime(2013, 2, 10, 0),
+					datetime(2013, 4, 10, 0),
+					datetime(2013, 4, 13, 0),
+					datetime(2013, 4, 23, 0),
+					datetime(2013, 4, 30, 0),
+					datetime(2013, 5, 7, 0),
+					datetime(2013, 5, 7, 0),
+					datetime(2013, 5, 12, 0),
+					datetime(2013, 5, 19, 0),
+					datetime(2013, 5, 24, 0),
+					datetime(2013, 5, 26, 0),
+					datetime(2013, 5, 26, 0),
+					datetime(2013, 5, 28, 0),
+					datetime(2013, 5, 29, 0),
+					datetime(2013, 5, 29, 0),
+					datetime(2013, 5, 30, 0),
+					datetime(2013, 6, 2, 0),
+					datetime(2013, 6, 2, 0),
+					datetime(2013, 6, 3, 0),
+					datetime(2013, 6, 3, 0),
+					datetime(2013, 6, 7, 0),
+					datetime(2013, 6, 7, 0),
+					datetime(2013, 6, 7, 0),
+					datetime(2013, 6, 9, 0),
+					datetime(2013, 6, 9, 0),
+					datetime(2013, 6, 10, 0),
+					datetime(2013, 6, 10, 0),
+					datetime(2013, 6, 10, 0),
+					datetime(2013, 6, 10, 0),
+					datetime(2013, 6, 11, 0),
+					datetime(2013, 6, 11, 0),
+					datetime(2013, 6, 12, 0),
+					datetime(2013, 6, 13, 0),
+					datetime(2013, 6, 13, 0),
+					datetime(2013, 6, 13, 0)])
 
+	return dates[test]
--- run.py
+++ run.py
@@ -1,16 +1,41 @@
+import numpy as np
+import sys
+import os
+import op
+import tracmass
+import netCDF4 as netCDF
+from mpl_toolkits.basemap import Basemap
+import pdb
+from matplotlib import delaunay
+from matplotlib.pyplot import *
+import glob
+from datetime import datetime, timedelta
+from mpl_toolkits.basemap import Basemap
+import time
+import tracpy
+import init
+from scipy import ndimage
 
+ntests = 40 # number of packages found
 
 # Read in parameter initialization
+loc, nsteps, ndays, ff, tseas, ah, av, z0, zpar, do3d, doturb = init.parameters()
 
-# Read in location initializations
-
-# Read in time initializations
-
-loc,nsteps,ndays,ff,tseas,ah,av,lon0,lat0,z0,zpar,do3d,doturb,name = init.parameters()
+# Read in grid
+grid = tracpy.inout.readgrid(loc,nc)
 
 # Loop through start locations and times for running different simulations
-for test in xrange(tests):
+for test in xrange(ntests):
+
+	# Read in location initializations
+	lon0, lat0, name = init.locations(test)
+
+	# Read in time initializations
+	date = init.start_times(test)
+
 	# TODO: Try to put each simulation on a different core of the current machine, except 1 or 2
-	lonp,latp,zp,t,grid = run(loc,nsteps,ndays,ff,date,tseas,ah,av,lon0,lat0,z0,zpar,do3d,doturb,name)
+	lonp, latp, zp, t, grid = run(loc, nsteps, ndays, ff, date, \
+									tseas, ah, av, lon0, lat0, \
+									z0, zpar, do3d, doturb, name)
 
 # Compile tex document with figures in it
